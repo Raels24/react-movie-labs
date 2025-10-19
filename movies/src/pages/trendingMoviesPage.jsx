aec80acc1f91385cd42632a859d7a518e22ada4f
sante <santersraels0@gmail.com>
2025-10-19 14:58:10 +0100

    Added Trending Today page.

---
+++ movies/src/pages/trendingMoviesPage.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import Spinner from "../components/spinner";
+import PageTemplate from "../components/templateMovieListPage";
+import { getTrendingMovies } from "../api/tmdb-api";
+import PlaylistAdd from "../components/cardIcons/playlistAdd";
+
+const trendingMoviesPage = () => {
+  const { data, error, isPending, isError } = useQuery({
+    queryKey: ["trending"],
+    queryFn: getTrendingMovies,
+  });
+
+  if (isPending) return <Spinner />;
+  if (isError) return <h1>{error.message}</h1>;
+
+  const movies = data.results;
+
+  return (
+    <PageTemplate
+      title="Trending Movies"
+      movies={movies}
+      action={(movie) => <PlaylistAdd movie={movie} />}
+    />
+  );
+};
+
+export default trendingMoviesPage;
